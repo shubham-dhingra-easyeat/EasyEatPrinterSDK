7c2b1990fe035276b6da755e4cb13136699eb0e0
Shubham Dhingra <shubham.dhingra@easyeat.ai>
2022-05-19 16:06:52 +0530

    Initial Commit

---
+++ EasyEatPrinterSDK/EasyEatPrinterSDK.h
@@ -0,0 +1,18 @@
+//
+//  EasyEatPrinterSDK.h
+//  EasyEatPrinterSDK
+//
+//  Created by Shubham Dhingra on 19/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyEatPrinterSDK.
+FOUNDATION_EXPORT double EasyEatPrinterSDKVersionNumber;
+
+//! Project version string for EasyEatPrinterSDK.
+FOUNDATION_EXPORT const unsigned char EasyEatPrinterSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyEatPrinterSDK/PublicHeader.h>
+
+
